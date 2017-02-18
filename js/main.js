cb29f01dd4312347ab64fb7baf4e58d272a70bb1
Neil Fowler <neil.mario70@yahoo.com>
2017-02-19 01:44:13 +0530

    addedNavBar icon toggle

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 $(document).ready(function(e) {
 	
 		$('.logo_mobile_menu').on('click', function(){
-		$('.header_level2 ul').toggleClass("open");
+			var hamIcon = $(this).find('i');
+			
+			if (hamIcon.hasClass('fa fa-bars') ){
+				hamIcon.removeClass('fa fa-bars').addClass('fa fa-times');
+				} else {
+					hamIcon.removeClass('fa fa-times').addClass('fa fa-bars');
+				}
+				
+			$('.header_level2 ul').toggleClass("open");
 		});
 	  
 });
